528f754441148c4710d5d70562f44846c6f8438b
Arthur Martins Alves <oarthurma@gmail.com>
2026-04-27 01:14:23 -0300

    fix: corrige geração de número aleatório para inteiros

--- app.js
+++ app.js
@@ -9,5 +9,7 @@ function sortear() {
 }
 
 function obterNumeroAleatorio(min, max) {
-  return Math.random() * (max - min) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+// 8:03

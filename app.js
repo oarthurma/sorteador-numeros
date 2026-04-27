2c3fc86ddc0c11171e752d7953afbb1401b5c95b
Arthur Martins Alves <oarthurma@gmail.com>
2026-04-27 04:24:01 -0300

    fix: evita números repetidos no sorteio

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ function sortear() {
 
   for (let i = 0; i < quantidade; i++) {
     numero = obterNumeroAleatorio(de, ate);
+
+    while (sorteados.includes(numero)) {
+      numero = obterNumeroAleatorio(de, ate);
+    }
+
     sorteados.push(numero);
   }
 

867734ba4baa076bb44f757fe73bed4869598438
Arthur Martins Alves <oarthurma@gmail.com>
2026-04-27 01:22:10 -0300

    refactor: remove alerts e prepara lógica de sorteio

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ function sortear() {
   let de = parseInt(document.getElementById('de').value);
   let ate = parseInt(document.getElementById('ate').value);
 
-  let numero = obterNumeroAleatorio(de, ate);
-  alert(numero)
+  let sorteados = [];
+  let numero;
 
+  for (let i = 0; i < quantidade; i++) {
+    numero = obterNumeroAleatorio(de, ate);
+    sorteados.push(numero);
+  }
+
+  alert(sorteados);
 }
 
 function obterNumeroAleatorio(min, max) {

d70b263d05d38f9fb3760e5c33d2eb424efbe0a2
Arthur Martins Alves <oarthurma@gmail.com>
2026-04-28 05:28:00 -0300

    feat: adiciona validação para impedir quantidade acima do intervalo disponível

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ function sortear() {
     return;
   }
 
+  if (quantidade > (ate - de + 1)) {
+    alert('Campo "Quantidade de números" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!')
+    return;
+  }
+
   let sorteados = [];
   let numero;
 
@@ -16,6 +21,7 @@ function sortear() {
 
     while (sorteados.includes(numero)) {
       numero = obterNumeroAleatorio(de, ate);
+      alert('Tentando obter número inédito');
     }
 
     sorteados.push(numero);

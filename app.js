b3b79409623c0a11bb347d1238e9cc0543a6d248
Arthur Martins Alves <oarthurma@gmail.com>
2026-04-28 04:33:12 -0300

    feat: implementa reinicialização completa do sorteador

--- app.js
+++ app.js
@@ -36,3 +36,12 @@ function alterarStatusBotao() {
   }
 }
 
+function reiniciar() {
+  document.getElementById('quantidade').value = '';
+  document.getElementById('de').value = '';
+  document.getElementById('ate').value = '';
+  document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+  alterarStatusBotao();
+
+}
+
